4cf7d5a4d02934e5e03828f43fdc429f87f1d70e
unknown <althierno@yahoo.fr>
2020-03-20 18:23:45 +0100

    first one

---
+++ pages/Global/animal.php
@@ -0,0 +1,85 @@
+<?php include("../Commons/header.php");?>
+
+<?= styleTitreNiveau1("Odin", COLOR_PENSIONNAIRE) ?>
+<div class='row border border-dark rounded-lg m-2 align-items-center perso_bgGreen'>
+            <div class="col p-2 text-center">
+                <img src='../../sources/images/Animaux/chats/Odin/Odin.jpg' class="img-thumbnail" style="max-height:180px;" alt="Framboise" />
+            </div>
+            <div class="col-2 col-md-1 border-left border-right border-dark text-center">
+                <img src='../../sources/images/Autres/icones/ChienOk.png' class="img-fluid m-1" style="width:50px;" alt="chienOk" />
+                <img src='../../sources/images/Autres/icones/ChatOk.png' class="img-fluid m-1" style="width:50px;" alt="chatOk" />
+                <img src='../../sources/images/Autres/icones/BabyOk.png' class="img-fluid m-1" style="width:50px;" alt="bayOk" />
+            </div>
+            <div class="col-6 col-md-4 text-center">
+                <div class="mb-2">Puce : XXXXXXXX</div>
+                <div class="mb-2">Né : XX/XX/XXXX</div>
+                <div class="my-3">
+                    <span class="badge badge-warning m-1 p-2 "> douce </span>
+                    <span class="badge badge-warning m-1 p-2 "> calme </span>
+                    <span class="badge badge-warning m-1 p-2 "> joueuse </span>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">
+                Frais d'adoption : 60 € <br/>
+                Vaccins : 35€ (à la demande de l'adoptant) <br/>
+                Stérilisation : caution de 200 € vous sera demandée (rendue après réception 
+                du certificat).
+              
+            </div>
+ </div>
+ <div class="row no-gutters align-items-center">
+ 	<div class="col-12 col-lg-6">
+ 		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active bg-dark"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1" class="bg-dark"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2" class="bg-dark"></li>
+  </ol>
+  <div class="carousel-inner text-center">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../../sources/images/Animaux/Chats/Odin/Odin7.jpg" class="img-thumbnail" style="height:400px" />
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../../sources/images/Animaux/Chats/Odin/Odin4.jpg" class="img-thumbnail" style="height:400px"/>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../../sources/images/Animaux/Chats/Odin/Odin5.jpg" class="img-thumbnail" style="height:400px">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+ </div>
+ <div class="col-12 col-lg-6 mr-auto">
+   <div>
+   	<?= styleTitreNiveau2("Qui-suise je ?", COLOR_PENSIONNAIRE) ; ?>
+   	ODIN est un chien de 4 semaines,310 gr atteint d'un herpès. Notre vétérinaire a dû lui faire une intervention
+   	chirurgicale au niveaux des yeux. Vous voulez en savoir plus sur notre adorable ODIN ?, euh ben c'est simple, ma gueule..... <br/> <br/>
+   	Vous pouvez suivre son <span class="badge badge-primary"> PARCOURS </span> 
+   	dans la section <a href="../Global/actu.php" target="_blank">Nouvelles des adoptés</a>
+   </div>
+   <hr/>
+   <div>
+    <img src="../../sources/images/Autres/icones/IconeAdopt.png" alt="" width="50" height="50" class="img-thumbnail"/>
+    Odin est la mascotte de l'association et est désormais dans sa famille pour la vie ! <br/> Donc après discussion avec les membres de l'association, et surtout avec la proposition de notre secrétaire de le prendre en famile d'accueil longue durée, Merci à toi !
+   </div>
+   <hr/>
+   <div>
+    <img src="../../sources/images/Autres/icones/oeil.jgp" alt="" width="50" height="50" class="img-thumbnail"/>
+   AGATE se trouve sur le scecteur de Saint Girons 09. Pas de co-voiturage possible.
+   </div>
+ </div>
+ <hr/>
+   <div>
+    <img src="../../sources/images/Autres/icones/iconeContrat.png" alt="" width="50" height="50" class="img-thumbnail"/>
+   Nous vous demandons de bien réfléchir aux engagements que vous allez devoir prendre envers ce petit choux. Un animal n'est pas <span class=" badge badge-warning"> UN JOUET </span>. Si on prend, c'est bien évidemment pour s'en occuper humainement.
+   </div>
+ </div>
+
+<?php include("../Commons/footer.php"); ?>
